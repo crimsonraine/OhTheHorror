a39dad1e8ae11ee8a3ae85bcd0da06f57e0d400b
crimsonraine <wukimberley98@gmail.com>
2023-12-02 16:40:45 -0500

    some sounds ig

---
+++ Assets/Scripts/FootstepSounds.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FootstepSounds : MonoBehaviour
+{
+    public AudioSource walkSource;
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D))
+        {
+            walkSource.enabled = true;
+        }
+        else
+        {
+            walkSource.enabled = false;
+        }
+    }
+}
