9cc4b27d1bdfa7262b533b325d94bd1240c57767
ishaaty <ishatyagi888@gmail.com>
2023-12-02 16:41:57 -0500

    began adding UI elements

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,13 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
+    public TextMeshProUGUI titleText;
+    public Button beginBtn;
+
+    public TextMeshProUGUI welcomeText;
+    public TextMeshProUGUI storyText;
+    public Button nextBtn;
+
+
+    public Button startGameBtn;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        titleText.gameObject.SetActive(true);
+        beginBtn.gameObject.SetActive(true);
     }
 
     // Update is called once per frame
@@ -15,4 +28,28 @@ public class GameManager : MonoBehaviour
     {
         
     }
+
+    // called by startBtn
+    public void DisplayStory(){
+        // removing previous UI Elements
+        titleText.gameObject.SetActive(false);
+        beginBtn.gameObject.SetActive(false);
+
+        // adding new UI Elements
+        welcomeText.gameObject.SetActive(true);
+        storyText.gameObject.SetActive(true);
+        nextBtn.gameObject.SetActive(true);
+    }
+
+    // called by nextBtn
+    public void DisplayInstructions(){
+        // removing previous UI Elements
+        welcomeText.gameObject.SetActive(true);
+        storyText.gameObject.SetActive(true);
+        nextBtn.gameObject.SetActive(true);
+
+        // adding new UI Elements
+    }
+
+    
 }
