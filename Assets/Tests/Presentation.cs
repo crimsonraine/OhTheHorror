e6af07bf664e8bcfda7eae44d828737ed57ac108
Kimberley Ni <wukimberley98@gmail.com>
2023-12-04 21:42:45 -0500

    maybe

--- Assets/Tests/Presentation.cs
+++ Assets/Tests/Presentation.cs
@@ -7,19 +7,25 @@ using UnityEngine.TestTools;
 public class Presentation
 {
     // A Test behaves as an ordinary method
-    [Test]
-    public void PresentationSimplePasses()
-    {
-        // Use the Assert class to test conditions
-    }
+    // [Test]
+    // public void TestOpenCloseAngle()
+    // {
+    //     // Use the Assert class to test conditions
+    // }
 
     // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
     // `yield return null;` to skip a frame.
     [UnityTest]
-    public IEnumerator PresentationWithEnumeratorPasses()
+    public IEnumerator RotationTester()
     {
-        // Use the Assert class to test conditions.
-        // Use yield to skip a frame.
-        yield return null;
+        var gameObject = new GameObject("DoorObject");
+        var doorLikeObj = gameObject.AddComponent<Door>();
+        float baseRotation = doorLikeObj.transform.rotation.eulerAngles.y;
+
+        doorLikeObj.GetComponent<Door>.Open();
+
+        yield return new WaitForSeconds(1f);
+
+        Assert.AreEqual(baseRotation + 90, doorLikeObj.transform.rotation.eulerAngles.y);
     }
 }
